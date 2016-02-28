5af1e398748060a93fab77ce9f7655c0e2f7570e
Konflict <Konflict@Konflicts-MacBook-Pro.local>
2016-02-28 14:04:06 -0800

    Finished mandatory tasks

---
+++ read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while read line
+do
+    if echo $line | grep -q HEAD
+    then
+	((HEAD++))
+	
+    elif echo $line | grep -q GET
+    then
+	((GET++))
+	  
+    fi
+done < $1
+
+printf "%d\n" $HEAD
+printf "%d\n" $GET
+
+	 
+	     
+	     
+	     
+	     
+	
